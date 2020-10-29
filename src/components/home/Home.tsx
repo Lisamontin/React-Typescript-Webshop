bd4c4731b1a76de7023d7380982eb626ba02f0db
Lisamontin <42090473+Lisamontin@users.noreply.github.com>
2020-10-29 09:55:13 +0100

    add api call and html to display data

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Home.scss'
 
-export default function Start() {
+interface IProducts {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+  year: number;
+  // productCategory: <create interface?>;
+};
 
+
+export default function Home() {
+
+  const defaultValue:IProducts [] = [];
+
+  //variable to hold the state for the posts from api
+
+  const [products, setProducts] = useState(defaultValue);
+
+
+
+  useEffect(() => {
+    axios.get('https://medieinstitutet-wie-products.azurewebsites.net/api/products')
+      .then(response => setProducts(response.data));
+      
+    }, []);
+
+  let productsHtml = products.map((product: IProducts) => {
+    return (
+        <div className="product" key={product.id}>
+          <dt>
+            <img className ="product-image" src={product.imageUrl} alt="product-image"/>
+          </dt>
+            <span className="product-name">
+              {product.name}
+            </span>
+          {/* <dd>{product.description}</dd> */}
+        <button type="button">Add to cart</button>
+      </div>
+    )
+  });
 
   return(
     <div>
       <h1>HOME component</h1>
+      <a href="#">{productsHtml}</a>
     </div>
   )
+
 }
