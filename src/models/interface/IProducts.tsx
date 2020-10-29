aeaf0147c8a5958093099b1164ccc9fca0bddcf9
Lisamontin <42090473+Lisamontin@users.noreply.github.com>
2020-10-29 11:08:06 +0100

    add interface dir

---
+++ src/models/interface/IProducts.tsx
@@ -0,0 +1,12 @@
+
+
+export default interface IProducts {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+  year: number;
+  // productCategory: <create interface?>;
+};
+
