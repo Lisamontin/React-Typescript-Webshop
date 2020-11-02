0362a802521311fb09f080a14eda675df1508fbe
Lisamontin <42090473+Lisamontin@users.noreply.github.com>
2020-11-02 16:16:36 +0100

    add api call and useparams

--- src/components/product-detail/ProductDetail.tsx
+++ src/components/product-detail/ProductDetail.tsx
@@ -1,24 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+
+import IProducts from '../../models/interface/IProducts';
 
 
-
+interface IParams{
+  id: string;
+}
+const defaultValue:IProducts = {id:0, name:'', description:'', price:0, imageUrl:'', year:0};
 
 export default function ProductDetail() {
+  let { id } = useParams<IParams>();
+  
+  const [product, setProduct] = useState(defaultValue);
 
-  // //ta emot info om vilken produkt som ska visas
-  // const {id} = useParams;
-  // const [ products, setProducts ] = //useContesx or try llifting state up here
+  useEffect(() => {
+    axios.get(`https://medieinstitutet-wie-products.azurewebsites.net/api/products/${id}`)
+      .then(response => setProduct(response.data));
 
-  // //
+    }, []);
 
+console.log(id);
   return(
-    <div>
-      <h1>Product detail Component</h1>
 
+
+    <div>
+      <h1> Product detail Component</h1>
+      <h3></h3>
+  <h3>{product.name}</h3>
+    <img src={product.imageUrl}></img>
       <button>add to cart</button>
       <button>Go back to products</button>
       
     </div>
+
   )
 }
